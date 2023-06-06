5d56514869be5781aafac6c21c8f45922828b318
rodrigo <hamuyrodrigo@gmail.com>
2023-06-06 20:26:47 +0100

    add ladle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import 'maplibre-gl/dist/maplibre-gl.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    Ola
   </React.StrictMode>,
 )
