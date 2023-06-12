1e7e31c57da26f63239c35fc9dc0802bf9c11a64
rodrigo <hamuyrodrigo@gmail.com>
2023-06-12 19:13:44 +0100

    prepping for first publish

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        "@react-three/fiber",
+        "maplibre-gl",
+        "react",
+        "react-dom",
+        "react-map-gl",
+        "three",
+      ]
+    },
+  },
 })
