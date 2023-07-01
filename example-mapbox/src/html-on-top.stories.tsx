49d92e60ca7dc85ee64e2f4fb68104f1a70ccc59
rodrigo <hamuyrodrigo@gmail.com>
2023-07-01 16:30:02 +0100

    add mapbox example

---
+++ example-mapbox/src/html-on-top.stories.tsx
@@ -0,0 +1,25 @@
+import { Box, Html, Stats } from "@react-three/drei";
+import { useState } from "react";
+import { MathUtils } from "three";
+import { Canvas } from "react-three-map";
+import { StoryMap } from "./story-map";
+
+export function Default() {
+
+  const [hovered, hover] = useState(false);
+
+  return <StoryMap latitude={51} longitude={0} zoom={13}>
+    <Canvas latitude={51} longitude={0} frameloop="demand">
+      <Html>ola</Html>
+      <Box
+        args={[500, 500, 500]}
+        position={[0, 250, 0]}
+        rotation={[0, 45 * MathUtils.DEG2RAD, 0]}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}
+        material-color={hovered ? 'purple' : 'orange'}
+      />
+      <Stats />
+    </Canvas>
+  </StoryMap>
+}
---
+++ example-mapbox/src/story-map.tsx
@@ -0,0 +1,46 @@
+import { ThemeState, useLadleContext } from '@ladle/react';
+import { useControls } from 'leva';
+import Mapbox from "mapbox-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { FC, PropsWithChildren } from "react";
+import Map from 'react-map-gl';
+
+export interface StoryMapProps extends PropsWithChildren {
+  latitude: number,
+  longitude: number,
+  zoom?: number,
+  pitch?: number,
+}
+
+/** `<Map>` styled for stories */
+export const StoryMap: FC<StoryMapProps> = ({
+  latitude, longitude, zoom = 18, pitch = 60, children
+}) => {
+
+  const { mapboxToken } = useControls({ mapboxToken: import.meta.env.VITE_MAPBOX_KEY })
+
+  const theme = useLadleContext().globalState.theme;
+
+  const mapStyle = theme === ThemeState.Dark
+    ? "mapbox://styles/mapbox/dark-v11"
+    : "mapbox://styles/mapbox/streets-v12";
+
+  Mapbox.accessToken = mapboxToken;
+
+  return <div style={{ height: '100vh', position: 'relative' }}>
+    {!mapboxToken && <>Add a mapbox token to load this component</>}
+    {mapboxToken && <Map
+      antialias
+      initialViewState={{
+        latitude,
+        longitude,
+        zoom,
+        pitch,
+      }}
+      mapStyle={mapStyle}
+      mapboxAccessToken={mapboxToken}
+    >
+      {children}
+    </Map>}
+  </div>
+}
