f38189a845b6cacc890a2d696109456c7345077b
rodrigo <hamuyrodrigo@gmail.com>
2023-07-01 23:06:55 +0100

    avoid optmizeDeps

--- example-maplibre/vite.config.ts
+++ example-maplibre/vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['react-three-map']
-  }
+  plugins: [react()]
 })
