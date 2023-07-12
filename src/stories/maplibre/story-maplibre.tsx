d0f2016d7fa6ee070ad38c25b377c277770574a1
rodrigo <hamuyrodrigo@gmail.com>
2023-07-12 13:24:11 +0100

    improve IFC story

--- src/stories/maplibre/story-maplibre.tsx
+++ src/stories/maplibre/story-maplibre.tsx
@@ -1,8 +1,8 @@
 import { ThemeState, useLadleContext } from '@ladle/react';
 import MapLibre from "maplibre-gl";
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { FC } from "react";
-import Map from 'react-map-gl/maplibre';
+import { FC, memo, useEffect, useRef } from "react";
+import Map, { useMap } from 'react-map-gl/maplibre';
 import { StoryMapProps } from '../story-map';
 import { Canvas } from 'react-three-map/maplibre';
 
@@ -25,9 +25,37 @@ export const StoryMaplibre: FC<StoryMapProps> = ({
       maxPitch={rest.pitch ? Math.min(rest.pitch, 85) : undefined}
       mapStyle={mapStyle}
     >
+      <FlyTo latitude={latitude} longitude={longitude} />
       <Canvas latitude={latitude} longitude={longitude} {...canvas}>
         {children}
       </Canvas>
       </Map>
   </div>
 }
+
+interface FlyToProps {
+  latitude: number,
+  longitude: number,
+}
+
+const FlyTo = memo<FlyToProps>(({latitude, longitude})=>{
+  
+  const map = useMap();
+  const firstRun = useRef(true);
+
+  useEffect(()=>{
+    if(!map.current) return;
+    if(firstRun.current) return;
+    map.current.flyTo({
+      center: [longitude, latitude],
+      maxDuration: .3,
+    })
+  }, [map, latitude, longitude])
+
+  useEffect(()=>{
+    firstRun.current = false;
+  }, [])
+
+  return <></>
+})
+FlyTo.displayName = 'FlyTo';
