adc35230a65ef6f44d11ceabdf583b79a93217f4
Rodrigo Hamuy Aquino <hamuyrodrigo@gmail.com>
2023-07-15 17:59:09 +0100

    fixes for stories sandbox

--- stories/vite.config.ts
+++ stories/vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import {resolve} from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-three-map/maplibre': resolve(__dirname, '../src/maplibre.index.ts'),
-      'react-three-map': resolve(__dirname, '../src/mapbox.index.ts'),
-    }
-  },
   optimizeDeps: {
     exclude: ['react-three-map']
   }
