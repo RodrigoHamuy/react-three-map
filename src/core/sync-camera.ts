0f1e2ffe1292bddf9f766bfab264f89611033fed
Rodrigo Hamuy Aquino <hamuyrodrigo@gmail.com>
2023-07-20 09:20:13 +0100

    custom billboard

--- src/core/sync-camera.ts
+++ src/core/sync-camera.ts
@@ -1,8 +1,40 @@
-import { Matrix4, Matrix4Tuple, PerspectiveCamera } from "three";
+import { Euler, MathUtils, Matrix4, Matrix4Tuple, PerspectiveCamera, Quaternion, Vector3 } from "three";
 
 const mx = new Matrix4();
+
+const q = new Quaternion().setFromEuler(new Euler(180*MathUtils.DEG2RAD,0,0))
+
 
 export function syncCamera(camera: PerspectiveCamera, origin: Matrix4Tuple, mapCamMx: Matrix4Tuple) {
   camera.projectionMatrix.fromArray(mapCamMx).multiply(mx.fromArray(origin));
   camera.projectionMatrixInverse.copy(camera.projectionMatrix).invert();
+  // const dir = projInvMxToDirection(camera.projectionMatrixInverse);
+  // camera.quaternion.matr4
+  // dirToQuaternion(dir,camera.quaternion);
+  // camera.projectionMatrixInverse.decompose(camera.position, camera.quaternion, camera.scale);
+  // camera.quaternion.multiply(q)
+
+  // console.log(camera.position.toArray())
+}
+
+const origin = new Vector3();
+const dir = new Vector3();
+
+/** from a projection matrix, return a direction vector in world coords */
+function projInvMxToDirection(invProj: Matrix4) {
+  origin.setScalar(0).applyMatrix4(invProj);
+  dir.set(0,0,1)
+  .applyMatrix4(invProj)
+  .sub(origin)
+  .normalize();
+  return dir;
+}
+
+const up = new Vector3(0,1,0);
+
+/** receive a direction vector and use it to create a Quaternion that will make an object look at that direction */
+function dirToQuaternion(dir: Vector3, quat: Quaternion) {
+  quat.identity();
+  quat.setFromUnitVectors(up, dir);
+  return quat;
 }
--- stories/src/billboard.stories.tsx
+++ stories/src/billboard.stories.tsx
@@ -1,21 +1,79 @@
-import { Billboard, Box } from "@react-three/drei";
-import { useState } from "react";
-import { MathUtils } from "three";
+import { Billboard, Box, Plane, Text } from "@react-three/drei";
+import { useRef, useState } from "react";
+import { AxesHelper, DoubleSide, MathUtils, Mesh, Object3D, Vector3 } from "three";
 import { StoryMap } from "./story-map";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
 
 export function Default() {
 
   const [hovered, hover] = useState(false);
 
-  return <StoryMap latitude={51} longitude={0} zoom={13} pitch={60}>
-    {/* <Billboard></Billboard> */}
-    <Box
-      args={[500, 500, 500]}
-      position={[0, 250, 0]}
-      rotation={[0, 45 * MathUtils.DEG2RAD, 0]}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}
-      material-color={hovered ? 'purple' : 'orange'}
-    />
+  return <StoryMap latitude={51} longitude={0} zoom={13} pitch={0}>
+    {/* <Text fontSize={17} color="#2592a8" scale={10} rotation={[-90*MathUtils.DEG2RAD,0,0]}>0</Text>
+    <Text fontSize={17} color="#2592a8" scale={10} rotation={[-90*MathUtils.DEG2RAD,0,0]} position={[0,3000,0]}>3000</Text> */}
+    <MyBox />
+    <axesHelper args={[300]} position={[300,0,0]}/>
+    {/* <Billboard>
+      <Plane
+        args={[500, 500, 500]}
+        position={[0, 250, 0]}
+        rotation={[0, 45 * MathUtils.DEG2RAD, 0]}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}
+        material-color={hovered ? 'purple' : 'orange'}
+      />
+      <Box
+        args={[200, 200, 200]}
+        position={[0, 50, 0]}
+        rotation={[0, 0, 0]}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}
+        material-color={hovered ? 'purple' : 'orange'}
+      />
+    </Billboard> */}
   </StoryMap>
+}
+
+const MyBox = () => {
+  const ref = useRef<Mesh>(null)
+  const cam = useThree(s=>s.camera);
+  const {follow} = useControls({follow: true});
+  const lookAt = useRef<AxesHelper>(null);
+  useFrame(()=>{
+    if(!ref.current) return;
+    if(!lookAt.current) return;
+    if(!follow) return;
+    const center = new Vector3().applyMatrix4(cam.projectionMatrixInverse);
+    const top = new Vector3(0,1,0).applyMatrix4(cam.projectionMatrixInverse);
+    ref.current.up.copy(top).sub(center).normalize();
+    const pos = lookAt.current.position;
+    pos.copy(ref.current.position);
+    pos.applyMatrix4(cam.projectionMatrix);
+    pos.z -= .1;
+    pos.applyMatrix4(cam.projectionMatrixInverse);
+    // console.log(pos.toArray())
+    // console.log('useFrame');    
+    // console.log(ref.current.matrix.toArray())
+    // ref.current.quaternion.copy(cam.quaternion);
+    // ref.current.updateMatrix();
+    ref.current.lookAt(pos)
+    // console.log(ref.current.matrix.toArray())
+  })
+  return <>
+  <Box
+  ref={ref}
+  args={[200, 200, 200]}
+  position={[0, 50, 0]}
+  >
+  <Plane 
+  args={[300,100]}
+  material-color="pink"
+  rotation={[0,0,0]} 
+  // material-side={DoubleSide}
+  />
+    <axesHelper args={[300]} />
+  </Box>
+  <axesHelper args={[300]} ref={lookAt} />
+  </>
 }
