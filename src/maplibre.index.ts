747f6414ef2e9f78deb0289315ac682a93e7ca81
Rodrigo Hamuy Aquino <hamuyrodrigo@gmail.com>
2023-07-21 16:28:13 +0100

    add useMap

--- src/maplibre.index.ts
+++ src/maplibre.index.ts
@@ -1,3 +1,8 @@
+import { MapInstance } from 'react-map-gl/maplibre';
+import { useMap as useMapGeneric } from './api/use-map';
+
 export * from './maplibre/canvas';
 export * from './api/canvas-props';
 export * from './api/coordinates';
+
+export const useMap = useMapGeneric<MapInstance>;
