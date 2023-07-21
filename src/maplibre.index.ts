9eb49a8244b1f74f6c7cb947416da0213baed5c7
Rodrigo Hamuy Aquino <hamuyrodrigo@gmail.com>
2023-07-21 16:55:24 +0100

    better types

--- src/maplibre.index.ts
+++ src/maplibre.index.ts
@@ -1,8 +1,8 @@
-import { MapInstance } from 'react-map-gl/maplibre';
+import type { Map } from 'maplibre-gl';
 import { useMap as useMapGeneric } from './api/use-map';
 
 export * from './maplibre/canvas';
 export * from './api/canvas-props';
 export * from './api/coordinates';
 
-export const useMap = useMapGeneric<MapInstance>;
+export const useMap = useMapGeneric<Map>;
