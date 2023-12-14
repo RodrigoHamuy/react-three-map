af87ed965c25ac8936d468430914cae760783b30
Rodrigo Hamuy Aquino <hamuyrodrigo@gmail.com>
2023-12-14 17:45:17 +0000

    show 3D buildings

--- src/api/use-map.ts
+++ src/api/use-map.ts
@@ -2,4 +2,4 @@ import { useThree } from "@react-three/fiber";
 import { MapInstance } from "../core/generic-map";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useMap = <T extends MapInstance = MapInstance>(): T | undefined => useThree((s) => (s as any as { r3m?: {map: T}}).r3m?.map);
+export const useMap = <T extends MapInstance = MapInstance>(): T => useThree((s) => (s as any as { r3m: {map: T}}).r3m.map);
