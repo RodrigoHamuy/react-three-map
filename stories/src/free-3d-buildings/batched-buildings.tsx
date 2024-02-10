9c301472c44cc2316ba6bf77b1ec47acf8df2b58
Rodrigo Hamuy Aquino <rodrigo@neobim.ai>
2024-02-10 11:40:06 +0000

    add batched material

--- stories/src/free-3d-buildings/batched-buildings.tsx
+++ stories/src/free-3d-buildings/batched-buildings.tsx
@@ -1,10 +1,11 @@
+import { Object3DNode, extend, useFrame } from "@react-three/fiber";
 import { memo, useLayoutEffect, useMemo, useRef } from "react";
-import { OverpassElement, getBuildingsData } from "./get-buildings-data";
-import { Vector3Tuple, Shape, ExtrudeGeometry, Matrix4, BatchedMesh, MathUtils } from "three";
 import { Coords, coordsToVector3 } from "react-three-map";
-import { extend, Object3DNode } from "@react-three/fiber";
-import { BatchedStandardMaterial } from "./batched-standard-material/batched-standard-material";
 import { suspend } from "suspend-react";
+import { BatchedMesh, Color, ExtrudeGeometry, MathUtils, Shape, Vector3Tuple } from "three";
+import { BatchedStandardMaterial } from "./batched-standard-material/batched-standard-material";
+import { OverpassElement, getBuildingsData } from "./get-buildings-data";
+import { useFunction } from "../../../src/core/use-function";
 
 extend({ BatchedStandardMaterial })
 
@@ -15,12 +16,13 @@ declare module '@react-three/fiber' {
   }
 }
 
-const m4 = new Matrix4();
-
 interface BatchedBuildingsProps {
   buildingsCenter: Coords;
   origin: Coords;
 }
+
+const _color = new Color();
+
 export const BatchedBuildings = memo<BatchedBuildingsProps>(({ buildingsCenter, origin }) => {
   const buildings = suspend(() => {
     const start = { ...buildingsCenter };
@@ -31,11 +33,25 @@ export const BatchedBuildings = memo<BatchedBuildingsProps>(({ buildingsCenter, 
     end.longitude += .01;
     return getBuildingsData({ start, end });
   }, [buildingsCenter]);
-  
+
   const { data, vertexCount, indexCount } = useMemo(() => {
-    const data = buildings.map(element => {
+    const data = buildings.map((element, i) => {
       const geometry = geoElementToGeometry(element, origin);
+      const c0 = new Color().setHSL(rand(0, 0.05), rand(1, 1), rand(0.5, 0.7));
+      const c1 = new Color().setHSL(rand(0.5, 0.55), rand(1, 1), rand(0.5, 0.7));
+      const emissiveIntensity = rand(0, 1) < 0.05 ? 3.5 : 0;
+      const roughness = rand(0, 0.5);
+      const metalness = rand(0, 1);
+      const offset = rand(0, 2 * Math.PI);
+      const speed = rand(1, 3);
+      const value = offset;
       return {
+        i,
+        value,
+        c0,
+        c1,
+        speed,
+        emissiveIntensity, roughness, metalness,
         element,
         geometry,
         vertexCount: geometry.attributes.position.count,
@@ -47,24 +63,45 @@ export const BatchedBuildings = memo<BatchedBuildingsProps>(({ buildingsCenter, 
     return { data, vertexCount, indexCount };
   }, [origin, buildings])
 
-  const ref = useRef<BatchedMesh>(null);
+  const meshRef = useRef<BatchedMesh>(null);
+  const matRef = useRef<BatchedStandardMaterial>(null);
+
+  const step = useFunction((delta = 0) => {
+    if (!matRef.current) return;
+    const material = matRef.current;
+    for (const item of data) {
+      const { c0, c1, emissiveIntensity, roughness, metalness, speed } = item;
+      item.value += delta * speed;
+      const sinValue = Math.sin(item.value)
+      const color = _color.lerpColors(c0, c1, sinValue);
+      material.setValue(item.i, 'diffuse', ...color);
+      color.multiplyScalar(emissiveIntensity);
+      material.setValue(item.i, 'emissive', ...c1);
+
+      material.setValue(item.i, 'roughness', roughness);
+      material.setValue(item.i, 'metalness', metalness);
+    }
+
+  })
+
+  useFrame((_, delta) => step(delta))
 
   useLayoutEffect(() => {
-    if(!ref.current) return;
-    const mesh = ref.current;
+    if (!meshRef.current) return;
+    const mesh = meshRef.current;
     for (let i = 0; i < data.length; i++) {
-      const id= mesh.addGeometry(data[i].geometry);
-      mesh.setMatrixAt(id, m4);
+      mesh.addGeometry(data[i].geometry);
     }
+    step();
   }, [data]);
 
   return <batchedMesh
-    ref={ref}
+    ref={meshRef}
     args={[data.length, vertexCount, indexCount]}
     rotation={[-90 * MathUtils.DEG2RAD, 0, -90 * MathUtils.DEG2RAD]}
-    onClick={e=>console.log(data[e.batchId||0])}
+    onClick={e => console.log(data[e.batchId || 0])}
   >
-    <meshLambertMaterial attach="material" color="yellow" />
+    <batchedStandardMaterial ref={matRef} args={[data.length]} />
   </batchedMesh>;
 })
 
@@ -73,7 +110,7 @@ BatchedBuildings.displayName = 'BatchedBuildings';
 function geoElementToGeometry(element: OverpassElement, origin: Coords) {
   const poly = geoPolyToVectorPoly(element.geometry || [], origin);
   let height = parseFloat(element.tags?.height || '0');
-  if(!height) height = parseFloat(element.tags?.['building:levels'] || '1') * 3;
+  if (!height) height = parseFloat(element.tags?.['building:levels'] || '1') * 3;
   const base = parseFloat(element.tags?.min_height || '0');
   const geo = polygonToExtrudeGeo(poly, height, base);
   return geo;
@@ -96,4 +133,9 @@ function polygonToExtrudeGeo(poly: Vector3Tuple[], height: number, base: number)
   const geo = new ExtrudeGeometry(shape, { depth: height - base, bevelEnabled: false });
   geo.translate(0, 0, base);
   return geo;
+}
+
+function rand(min: number, max: number) {
+  const delta = max - min
+  return min + Math.random() * delta
 }
